9a2d85ca72086164d08f4646010b5ab2fb5d8175
jeffsummersdesigns <jeff-summers@live.com>
2018-04-25 09:43:02 -0700

    Commit all changes

---
+++ Selenium3_7_VisualStudio2017/GoogleSearchEngineUsingEdge.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Edge;
+
+namespace Selenium3_7_VisualStudio2017
+{
+    [TestClass]
+    public class GoogleSearchEngineUsingEdge
+    {
+        // Edge driver full path: @"C:\SeleniumEdgeDriver\MicrosoftWebDriver.exe"
+        string edgeDriverLocation = @"C:\SeleniumEdgeDriver";
+
+        [TestMethod]
+        public void Should_Search_Using_EdgeBrowser()
+        {
+            // Initialize the IE Driver
+            using (var driver = new EdgeDriver(edgeDriverLocation))
+            {
+                // 1. Maximize the browser
+                driver.Manage().Window.Maximize();
+
+                // 2. Go to the "Google" homepage
+                driver.Navigate().GoToUrl("http://www.google.com");
+
+                // 3. Find the search textbox (by ID) on the homepage
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+                var searchBox = driver.FindElementById("lst-ib");
+
+                // 4. Enter the text (to search for) in the textbox
+                searchBox.SendKeys("Automation using selenium 3.0 in C#");
+
+                // 5. Find the search button (by Name) on the homepage
+                var searchButton = driver.FindElementByName("btnK");
+
+                // 6. Click "Submit" to start the search
+                searchButton.Submit();
+
+                // 7. Find the "Id" of the "Div" containing results stats,
+                // just before the results table.
+                var searchResults = driver.FindElementById("resultStats");
+            }
+        }
+    }
+}
---
+++ Selenium3_7_VisualStudio2017/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Selenium3_7_VisualStudio2017")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Selenium3_7_VisualStudio2017")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9e3bffe1-bb82-48db-8f7b-c200403c4d7e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
